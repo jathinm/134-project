a9c95d54faf2e000ef5355fefd72b76a2f9189f8
jathinm <96036135+jathinm@users.noreply.github.com>
2022-04-25 12:25:27 +0530

    Add files via upload

---
+++ baby.js
@@ -0,0 +1,43 @@
+Status = "";
+baby_image = "";
+objects = [];
+
+function preload(){
+    baby_image = loadImage("baby.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315.200);
+    object_detector = ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+function modelLoaded(){
+ console.log("ModelLoaded");
+ Status = true;
+ object_detector.Detect(tv_image,gotResults);
+}
+
+function gotResults(results,error){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(tv_image,0,0,640,350);
+    if(Status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: object detected";
+
+            fill("#fc0303");
+            percent = floor(objects[i].confidence + 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].height);
+            noFill();
+            stroke("#fc0303");
+            rect(object[i].x, objects[i].width, objects[i].height);
+        }
+    }
+}
